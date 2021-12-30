ac8ab0dc54eb68bf194f96bef93a520a39616437
Yash Hatwar <yashhatwar40@gmail.com>
2021-12-31 02:22:08 +0530

    landing commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Landingsection from './Landing_section/landingsection';
+
+ import "./App.css";
+
+
+const App = () => {
+  return(
+     <main>
+    <Landingsection />
+      </main>
+  );
+};
+
+export default App;
